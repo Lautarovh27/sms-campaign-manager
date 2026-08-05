dd92ff099526375e3fa21b412e5bf95f36864dc2
Lautaro <lautarovanh@gmail.com>
2026-08-05 01:46:28 -0300

    chore: restrict CORS to frontend domains

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import indexRoutes from "./routes/index.routes.js";
 import contactsRoutes from "./routes/contacts.routes.js";
-import {errorHandler} from "./middlewares/error.middleware.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://sms-campaign-manager-nine.vercel.app"
+    ]
+}));
 app.use(express.json());
 
 app.use(indexRoutes);
